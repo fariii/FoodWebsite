3d67e43bfaefc071ddccb0afe3255b11bf897850
fariii <4972082@student.swin.edu.au>
2016-05-24 19:35:41 +1000

    Create bootstrap.php

---
+++ test/bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+
+if (!defined("SETTINGS_FILE")) {
+    define("SETTINGS_FILE", __DIR__ . "/../src/settings.php");
+}
+if (!defined("LOGIN_FILE")) {
+    define("LOGIN_FILE", __DIR__ . "/../src/login.php");
+}
+
+require_once "helperEval.php";
