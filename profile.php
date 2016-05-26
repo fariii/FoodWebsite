7844f4849f0ed14f70328ab853dbccafe49f16b4
fariii <4972082@student.swin.edu.au>
2016-05-26 16:24:47 +1000

    Update profile.php

--- profile.php
+++ profile.php
@@ -58,7 +58,10 @@
 					</label>
 				</p>
 
-				<p>	<input type="submit" name="submit" value="update" /></p>
+				<input type="submit" name="updating" value="update" />
+					<input type="submit" name="adding" value="add"/>
+				
+					
 			</form>
 		</section>
 
@@ -68,7 +71,7 @@
 			$conn = @mysqli_connect($host, $user, $pass,$db);
 			require_once('table.php');
 			
-			if ((isset($_POST["FoodName"])) && (isset($_POST["price"])) &&  (isset($_POST["description"])))
+			if ((isset($_POST["updating"])))
 			{
 				$FoodName = trim($_POST["FoodName"]);
 				$price = trim($_POST["price"]);
@@ -77,25 +80,54 @@
 
 				if ($FoodName && $price && $description) 
 				{
-				
-					if ($FoodName=="Meal1" || $FoodName=="Meal 2")
-					{
 
-							$query2 = "UPDATE menu SET price=$price WHERE FoodName='$FoodName'";
-							$query3 = "UPDATE menu SET description='$description' WHERE FoodName='$FoodName'";
-							$result2 = mysqli_query($conn,$query2);
-							$result3= mysqli_query($conn,$query3);
-
-							if ($result2 && $result3)
-							{
-								header("location: profile.php");
-							}
+					$query2 = "UPDATE menu SET price=$price WHERE FoodName='$FoodName'";
+					$query3 = "UPDATE menu SET description='$description' WHERE FoodName='$FoodName'";
+					$result2 = mysqli_query($conn,$query2);
+					$result3= mysqli_query($conn,$query3);
+					// checks if the execution was successful 
+					if ($result2 && $result3)
+					{ 
+						header("location: profile.php");
+					}
 					
-					}
-					else
-					{
-						echo "Meal does not exist";
-					}
+				} // are entered
+				else
+				{
+					echo "Please enter all the requirement.";
+				}
+			
+			} // end of isset
+
+
+
+
+
+			if ((isset($_POST["adding"])))
+			{
+
+				$FoodName = trim($_POST["FoodName"]);
+				$price = trim($_POST["price"]);
+				$description = trim($_POST["description"]);
+
+				if ($FoodName && $price && $description) 
+				{
+						$insert = "insert into menu (FoodName,price,description) 
+						values ('$FoodName','$price','$description')"; 
+						// execute the query -we should really check to see if the database exists first. 
+						$insert_result = mysqli_query($conn, $insert); 
+						// checks if the execution was successful 
+						if(!$insert_result)
+						{ 
+							echo "This meal already exists"; 
+						}
+						else
+						{ 
+							// display an operation successful message 
+							echo "<p class=\"ok\">'$FoodName' is added to the menu.</p>"; 
+							header("location: profile.php");
+
+						} // if successful query operation
 
 
 					
@@ -104,8 +136,12 @@
 				{
 					echo "Please enter all the requirement.";
 				}
-	
-			} // end of isset
+			}
+
+
+
+
+
 	 ?>
 </article>
 
